1a0d61b6595037cade532501f792ebd8f9e1e5d6
Stanislav <stanislaworonin@yandex.ru>
2022-11-18 09:53:45 +0300

    fix logic for get page with comments by post ID v5

--- src/dataMapping/commentOutputDataForAuthorisationUser.ts
+++ src/dataMapping/commentOutputDataForAuthorisationUser.ts
@@ -4,6 +4,11 @@ import {CommentBDConstructor, CommentsBDType} from "../types/comment-constructor
 export const commentOutputDataForAuthorisationUser = async (comment: CommentBDConstructor, userId: string): Promise<CommentBDConstructor> => {
     const userReaction = await userLikesRepository.giveUserLike(userId, comment.id)
 
+    let reaction = 'None'
+    if (userReaction) {
+        reaction = userReaction.likeStatus
+    }
+
     const likeInfo = await likesInfoRepository.giveLikeInfo(comment.id)
 
     return {
@@ -13,7 +18,7 @@ export const commentOutputDataForAuthorisationUser = async (comment: CommentBDCo
         userLogin: comment.userLogin,
         createdAt: comment.createdAt,
         likesInfo: {
-            myStatus: userReaction!.likeStatus,
+            myStatus: reaction,
             likesCount: likeInfo!.likesCount,
             dislikesCount: likeInfo!.dislikesCount
         }
