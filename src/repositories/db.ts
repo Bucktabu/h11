0f3b38279ec12eb2040165ade324d0f4d11a53f0
Stanislav <stanislaworonin@gmail.com>
2022-11-19 15:51:15 +0300

    fix build for vercel v3

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -2,15 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 import mongoose from 'mongoose';
-import {BlogConstructor} from "../types/blogs-constructor";
-import {CommentBDConstructor} from "../types/comment-constructor";
-import {DeviceSecurityConstructor} from "../types/deviceSecurity-constructor";
-import {EmailConfirmationConstructor} from "../types/emailConfirmation-constructor";
-import {PostConstructor} from "../types/posts-constructor";
-import {TokenConstructor} from "../types/token-constructor";
-import {UserDBConstructor} from "../types/user-constructor";
-import {UserIpAddressConstructor} from "../types/UserIpAddress-constructor";
-///?maxPoolSize=20&w=majority
+
 const mongoUri = process.env.MONGO_URI || 'mongodb://0.0.0.0:27017'
 const dbName = process.env.mongoDBName || 'blogPlatform'
 
